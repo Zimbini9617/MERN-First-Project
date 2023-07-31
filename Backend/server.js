f80842b69701b66eb2f510a57dca5f3648ec11cd
Zimbini9617 <126578349+Zimbini9617@users.noreply.github.com>
2023-07-31 16:57:59 +0200

    backend update

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const User = require('./models/userModel');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
+const router = require('./routes/userRoutes');
 
-app.listen(8080, () => {
- console.log('server is running on port 8080'); 
-});
+//get, post, put, delete
+dotenv.config();
+
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+mongoose
+  .connect(process.env.URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log('server is running in port', process.env.PORT);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use('/', router)
