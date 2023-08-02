eedb07c44ebf853a4275ba89197beb6e0824aa2a
Zimbini9617 <126578349+Zimbini9617@users.noreply.github.com>
2023-08-02 11:09:23 +0200

    update

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,39 @@
 import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Root from './components/Root';
+import ErrorPage from './error-page';
+import NewPost from './pages/NewPost';
+import AllPost from './pages/AllPost';
+import Home from './pages/Home';
+import Update from './pages/Update';
 
+
+const router = createBrowserRouter([
+  {
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/all-post",
+        element: <AllPost />,
+      },
+      {
+        path: "/new-post",
+        element: <NewPost />,
+      },
+      {
+        path: "/:id",
+        element: <Update />,
+      },
+    ],
+  },
+])
 const App = () => {
-  return (
-    <div>App</div>
-  )
+  return <RouterProvider router={router} />;
 }
 
 export default App
--- Frontend/src/components/Card.jsx
+++ Frontend/src/components/Card.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
-
-const Card = () => {
+import React from 'react';
+import { Link } from 'react-router-dom';
+const Card = ({ user, onDelete }) => {
+  const { name, email, age, _id } = user;
   return (
-    <div>
-      
+    <div className="flex-1">
+      <div className="border border-gray-500 rounded-md p-3">
+        <h1 className="text-xl">
+          <span>Name:</span>
+          {name}
+        </h1>
+        <p className="text-lg">
+          <span className="font-semibold">Age:</span>
+          {age}
+        </p>
+        <p className="text-lg">
+          <span className="font-semibold">email:</span>
+          {email}
+        </p>
+        <div>
+          <Link
+            to={`/${_id}`}
+            className="px-3 py-1 bg-green-800 rounded-md border border-white text-white uppercase hover:bg-green-300 my-4 mr-2"
+          >
+            Update
+          </Link>
+          <button
+            className="px-3 py-1 bg-red-700 rounded-md border border-white uppercase text-white hover:bg-red-500 my-4"
+            onClick={() => onDelete(_id)}
+          >
+            Delete
+          </button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
