486e61e87116ac995ccbdeb0b6368caeafe152b1
Zimbini9617 <126578349+Zimbini9617@users.noreply.github.com>
2023-08-02 13:35:15 +0200

    update

--- Frontend/src/pages/AllPost.jsx
+++ Frontend/src/pages/AllPost.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react';
+import Card from '../components/Card';
 const AllPost = () => {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState('');
+  const getData = async () => {
+    const response = await fetch('http://localhost:8000');
+
+    const result = await response.json();
+    if (!response.ok) {
+      setError(result.message);
+    }
+    if (response.ok) {
+      setData(result);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  });
+
+  const handleDelete = async (id) => {
+    const response = await fetch(`http://localhost:8000/${id}`, {
+      method: 'DELETE',
+    });
+    const result = response.json();
+
+    if (!response.ok) {
+      setError(result.message);
+    }
+    if (response.ok) {
+      setError('Deleted Sucessfully');
+    }
+    setTimeout(() => {
+      setError('');
+      getData();
+    }, 1000);
+  };
   return (
-    <div>AllPost</div>
-  )
-}
+    <div>
+      <div className="bg-red-800 text-center text-white font-bold uppercase">
+        {error && <h1>{error}</h1>}
+      </div>
+
+      <h1 className='font-bold font-mono text-green-950 text-2xl'>Show All Data:</h1>
+      <div className="flex gap-3 mt-4 flex-wrap">
+        {data?.map((user) => (
+          <Card user={user} key={user._id} onDelete={handleDelete} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default AllPost
